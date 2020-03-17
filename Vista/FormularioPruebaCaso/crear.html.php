bea9bad35467a485179e19981782ef72843673b0
Marlon Pinillo <stivenpinillocarabali@hotmail.com>
2020-03-17 11:20:03 -0500

    Se formaliza las entradas Modulo, ConfiguracionDeLaPrueba en Vista/FormularioPruebaCaso/crea.html.php y se añade la clase options en el css en Font-styles.css

--- Vista/FormularioPruebaCaso/crear.html.php
+++ Vista/FormularioPruebaCaso/crear.html.php
@@ -28,15 +28,23 @@
             <!--Start Row-->
             <div class="row">
                 <div class="col-sm">
-                    <span id="descripcionVersion">Version*</span><br>
-                    <select name="version" id="versionSCUno" data-live-search="true" class="selectpicker custom-select-sm form-control input-group" data-size="4" >
-                        <option value="#" class="options">Seleccion de version</option>
+                    <span id="descripcionVersion" class="labelCP">Modulo*</span><br>
+                    <select name="version" id="versionSCUno" data-live-search="true" class="selectpicker custom-select-sm form-control input-group" data-size="4">
+                        <option value="#" class="options">Seleccion del modulo</option>
                         <option value="0000" class="options">0000</option>
                         <option value="1111" class="options">1111</option>
                         <option value="2222" class="options">2222</option>
                         <option value="3333" class="options">3333</option>
                         <option value="4444" class="options">4444</option>
                         <option value="5555" class="options">5555</option>
+                    </select>
+                </div>
+                <div class="col-sm">
+                    <span id="descripcionVersion" class="labelCP">Configuracion de la prueba*</span><br>
+                    <select name="version" id="versionSCUno" data-live-search="true" class="selectpicker custom-select-sm form-control input-group" data-size="4">
+                        <option value="#" class="options">Seleccion el tipo de prueba</option>
+                        <option value="ActualPrueba" class="options">El usuario actual es responsable del proceso</option>
+                        <option value="OtroPrueba" class="options">Otro usuario sera responsable del proceso</option>
                     </select>
                 </div>
             </div>
