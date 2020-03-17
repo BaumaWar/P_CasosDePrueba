4ddc5186b1fd046fe71059e67a31b7daf8c43c86
Marlon Pinillo <stivenpinillocarabali@hotmail.com>
2020-03-17 10:24:14 -0500

    Se crea la interfaz para las entradas de NombrePrueba,NombreDelCreador,FechaDeCreacion y SeleccioneModulo en Vistas/FormularioPruebaCaso/crear.html.php

---
+++ Vista/FormularioPruebaCaso/crear.html.php
@@ -0,0 +1,49 @@
+<!--Start FomularioPruebaCaso Crear-->
+<div class="container bg-blanco posicion border bor-rad">
+    <!--Start Card-->
+    <div class="card bg-blanco border-0"> 
+        <!--Start CardBody-->
+        <div class="card-body">
+            <!--Start Title-->
+            <div class="text-center font-weight-normal p-10 labelHead">
+                PROCESO DE PRUEBA DE CASOS
+            </div>
+            <!--End Title-->
+            <!--Start Row-->
+            <div class="row">
+                <div class="col-sm">
+                    <span id="" class="labelCP">Nombre de prueba*</span><br>
+                    <input type="text" name="" id="" class="custom-select-sm form-control input-group" />
+                </div>
+                <div class="col-sm">
+                    <span id="" class="labelCP">Nombre del creador*</span><br>
+                    <input type="text" name="" id="" class="custom-select-sm form-control input-group" />
+                </div>
+                <div class="col-sm">
+                    <span id="" class="labelCP">Fecha de creacion*</span><br>
+                    <input type="text" name="" id="" class="custom-select-sm form-control input-group" />
+                </div>
+            </div>
+            <!--End Row-->
+            <!--Start Row-->
+            <div class="row">
+                <div class="col-sm">
+                    <span id="descripcionVersion">Version*</span><br>
+                    <select name="version" id="versionSCUno" data-live-search="true" class="selectpicker custom-select-sm form-control input-group" data-size="4" >
+                        <option value="#" class="options">Seleccion de version</option>
+                        <option value="0000" class="options">0000</option>
+                        <option value="1111" class="options">1111</option>
+                        <option value="2222" class="options">2222</option>
+                        <option value="3333" class="options">3333</option>
+                        <option value="4444" class="options">4444</option>
+                        <option value="5555" class="options">5555</option>
+                    </select>
+                </div>
+            </div>
+            <!--End Row-->
+        </div>
+        <!--End CardBody-->
+    </div>
+    <!--End Card-->
+</div>
+<!--End FomularioPruebaCaso Crear-->    
